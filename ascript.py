e2b7af3e54830adb5924b3c4c6176653375550fd
Tom Moroney <moroneyt@tcd.ie>
2023-05-03 02:27:27 +0100

    Text stretches to fill gaps

--- ascript.py
+++ ascript.py
@@ -1,5 +1,4 @@
 import sys
-import datetime
 import time
 
 projectManager = resolve.GetProjectManager()
@@ -23,50 +22,55 @@ else:
     file_path = r'S:\Blackmagic Design\DaVinci Resolve\Fusion\Scripts\Utility\audio.srt'
     with open(file_path, 'r') as f:
         lines = f.readlines()
+
+    # PARSE SRT FILE
+    subs = []
+    for i in range(0, len(lines), 4):
+        frame_rate = timeline.GetSetting("timelineFrameRate") # get timeline framerate
+        start_time, end_time = lines[i+1].strip().split(" --> ")
+        text = lines[i+2].strip()
+        # Convert the timestamp string to seconds
+        hours, minutes, seconds_milliseconds = start_time.split(':')
+        seconds, milliseconds = seconds_milliseconds.split(',')
+        frames = int(round((int(hours) * 3600 + int(minutes) * 60 + int(seconds) + int(milliseconds) / 1000) * frame_rate))
+        timelinePos = frames + timeline.GetStartFrame() # calculate time in frames
+        #print("Start Frame: ", timelinePos)
+        hours, minutes, seconds_milliseconds = end_time.split(':')
+        seconds, milliseconds = seconds_milliseconds.split(',')
+        frames = int(round((int(hours) * 3600 + int(minutes) * 60 + int(seconds) + int(milliseconds) / 1000) * frame_rate))
+        duration = frames - timelinePos # set duration of subtitle in frames
+        #print("End Frame: ", duration)
+        subs.append([timelinePos, duration, text])
     
+    # PUT TEXT+ ON TIMELINE
     folder = mediaPool.GetRootFolder()
     items = folder.GetClipList()
     subtitles = []
     for item in items:
         if item.GetName() == "Text+": # Find Text+ in Media Pool
-            for i in range(0, len(lines), 4):
-                frame_rate = timeline.GetSetting("timelineFrameRate") # get timeline framerate
-                start_time, end_time = lines[i+1].strip().split(" --> ")
-                text = lines[i+2].strip()
-                # Convert the timestamp string to seconds
-                hours, minutes, seconds_milliseconds = start_time.split(':')
-                seconds, milliseconds = seconds_milliseconds.split(',')
-                frames = int(round((int(hours) * 3600 + int(minutes) * 60 + int(seconds) + int(milliseconds) / 1000) * frame_rate))
-                timelinePos = frames + timeline.GetStartFrame() # calculate time in frames
-                print("Start Frame: ", timelinePos)
-
-                hours, minutes, seconds_milliseconds = end_time.split(':')
-                seconds, milliseconds = seconds_milliseconds.split(',')
-                frames = int(round((int(hours) * 3600 + int(minutes) * 60 + int(seconds) + int(milliseconds) / 1000) * frame_rate))
-                end_frame = frames - timelinePos # set duration in frames
-                print("End Frame: ", end_frame)
-
-                # CHANGE: USE DICT SO CAN CHECK NEXT TIME
-
-                # PUT TEXT+ ON TIMELINE
+            for i in range(len(subs)):
+                timelinePos, duration, text = subs[i]
+                if i < len(subs)-1 and subs[i+1][0] - (timelinePos + duration) < 200: # if gap between subs is less than 10 frames
+                    duration = subs[i+1][0] - subs[i][0] # set duration to next start frame
                 timelineTrack = 2 # set video track
                 newClip = {
                     "mediaPoolItem" : item,
                     "startFrame" : 0,
-                    "endFrame" : end_frame,
+                    "endFrame" : duration,
                     "trackIndex" : timelineTrack,
                     "recordFrame" : timelinePos
                 }
-                if mediaPool.AppendToTimeline( [newClip] ) : # Append text to timeline
+                if mediaPool.AppendToTimeline( [newClip] ) :    # Append text to timeline
                     print("Added clip to timeline: " + item.GetName())
-                    timeline.SetCurrentTimecode(timelinePos) # Move playhead to start of text
-                    resolve.OpenPage('fusion')               # Open the Fusion page
-                    fusion = resolve.Fusion()                # Call the Fusion Object
-                    cc = fusion.GetCurrentComp()             # Get the Current Fusion Composition
-                    template = cc.FindTool('Template')       # Find the Template Tool for Setting +aText
-                    cc.SetActiveTool(template)               # Set it to active
-                    template.SetInput('StyledText', text, 0) # Set the input of the StyledText to the variable text.
-                    resolve.OpenPage('edit')                 # Go Back to the Edit Page.
+                    timeline.SetCurrentTimecode(timelinePos)    # Move playhead to start of text
+                    resolve.OpenPage('fusion')                  # Open the Fusion page
+                    fusion = resolve.Fusion()                   # Call the Fusion Object
+                    cc = fusion.GetCurrentComp()                # Get the Current Fusion Composition
+                    template = cc.FindTool('Template')          # Find the Template Tool for Setting +aText
+                    cc.SetActiveTool(template)                  # Set it to active
+                    template.SetInput('StyledText', text, 0)    # Set the input of the StyledText to the variable text.
+                    resolve.OpenPage('edit')                    # Go Back to the Edit Page.
+                    if i % 4 == 0: projectManager.SaveProject() # Save Project every 4 Text+ added
             break # only execute once if multiple Text+ in Media Pool
 
 projectManager.SaveProject()
