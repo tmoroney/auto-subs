0e1442c162ea3ee882bfeab4022989f2b992c784
Tom Moroney <moroneyt@tcd.ie>
2024-10-05 21:24:20 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    basePath: "/auto-subs",
+    output: "export", // Enables static exports
+    reactStrictMode: true,
+};
+
+module.exports = nextConfig;
