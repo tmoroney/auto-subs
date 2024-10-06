0f4bf0ff8dd3e22b24898dae20bd3534d3e8ef3a
Tom Moroney <moroneyt@tcd.ie>
2024-10-06 15:38:28 +0100

    Updated design more

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
     basePath: "/auto-subs",
     output: "export", // Enables static exports
     reactStrictMode: true,
+    images: { unoptimized: true }
 };
 
 module.exports = nextConfig;
