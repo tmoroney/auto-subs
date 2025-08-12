b4c630d05f8e5901fc13c5441e049c439233e268
Tom Moroney <moroneyt@tcd.ie>
2025-08-12 03:44:46 +0100

    Revert "Changed model paths to latest again"
    This reverts commit e74b7d5695ebdee25e5c2950aca42874bcbb319d.

--- components/ui/DownloadModal.tsx
+++ components/ui/DownloadModal.tsx
@@ -41,15 +41,15 @@ export function DownloadModal({ isOpen, onClose }: DownloadModalProps) {
   const downloadOptions: DownloadOption[] = [
     {
       name: 'AutoSubs for Windows',
-      version: 'v3.0.1',
-      size: '68 MB',
+      version: 'v2.1.8',
+      size: '100 MB',
       os: 'Windows',
-      downloadUrl: 'https://github.com/tmoroney/auto-subs/releases/latest/download/AutoSubs-windows-x86_64.exe',
+      downloadUrl: 'https://github.com/tmoroney/auto-subs/releases/download/V2.1.8/AutoSubs-Win-setup.exe',
       isRecommended: currentOS === 'Windows'
     },
     {
       name: 'AutoSubs for macOS',
-      version: 'v3.0.1',
+      version: 'v3.0.0',
       size: '66 MB',
       os: 'macOS',
       downloadUrl: 'https://github.com/tmoroney/auto-subs/releases/latest/download/AutoSubs-macOS-arm64.pkg',
